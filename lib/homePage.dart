b5a9ac207dced27dd398501066ede0a5538514f7
ZheerBarzan <zheerbarzan85@outlook.com>
2023-08-24 19:38:46 +0300

    add the gesture detector to each individual cell

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
   @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  String displayXO = "";
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-      ),
+    return Scaffold(
+      backgroundColor: Colors.grey.shade800,
+      body: GridView.builder(
+          itemCount: 9,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemBuilder: ((context, index) {
+            return GestureDetector(
+              onTap: onTap,
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade700)),
+                child: Center(
+                  child: Text(
+                    displayXO,
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                ),
+              ),
+            );
+          })),
     );
+  }
+
+  void onTap() {
+    setState(() {
+      displayXO = "O";
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Homepage();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+    );
   }
 }
