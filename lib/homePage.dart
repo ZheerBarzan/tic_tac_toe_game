c8ee7e06c8487886e17085c1f0a128bedd510fa2
ZheerBarzan <zheerbarzan85@outlook.com>
2023-08-30 03:15:58 +0300

    add all the rules of the game

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -52,11 +52,58 @@ class _HomepageState extends State<Homepage> {
       },
     );
   }
+  //012
+  //345
+  //678
 
   void checkWinner() {
+    // checks the first row
     if (displayXO[0] == displayXO[1] &&
         displayXO[0] == displayXO[2] &&
         displayXO[0] != '') {
+      showWinDialog();
+    }
+    //checks the first column
+    if (displayXO[0] == displayXO[3] &&
+        displayXO[0] == displayXO[6] &&
+        displayXO[0] != '') {
+      showWinDialog();
+    }
+    //check diagonal one
+    if (displayXO[0] == displayXO[4] &&
+        displayXO[0] == displayXO[8] &&
+        displayXO[0] != '') {
+      showWinDialog();
+    }
+
+    ///check second column
+    if (displayXO[1] == displayXO[4] &&
+        displayXO[1] == displayXO[7] &&
+        displayXO[1] != '') {
+      showWinDialog();
+    }
+    //checks the third column
+    if (displayXO[2] == displayXO[5] &&
+        displayXO[2] == displayXO[8] &&
+        displayXO[2] != '') {
+      showWinDialog();
+    }
+    //checks the second diagonal
+    if (displayXO[2] == displayXO[4] &&
+        displayXO[2] == displayXO[6] &&
+        displayXO[2] != '') {
+      showWinDialog();
+    }
+    // checks the second row
+    if (displayXO[3] == displayXO[4] &&
+        displayXO[3] == displayXO[5] &&
+        displayXO[3] != '') {
+      showWinDialog();
+    }
+    //checks the third row
+    if (displayXO[6] == displayXO[7] &&
+        displayXO[6] == displayXO[8] &&
+        displayXO[6] != '') {
       showWinDialog();
     }
   }
