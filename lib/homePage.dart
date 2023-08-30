0359eb08c940216e4b500d972baa4d60bf4da518
ZheerBarzan <zheerbarzan85@outlook.com>
2023-08-30 20:34:02 +0300

    add the score board to the game

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -15,28 +16,53 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade800,
-      body: GridView.builder(
-        itemCount: 9,
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-            onTap: () {
-              _onTap(index);
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey.shade700)),
-              child: Center(
-                child: Text(
-                  displayXO[index],
-                  style: const TextStyle(color: Colors.white, fontSize: 40),
-                ),
+      body: Column(children: <Widget>[
+        Expanded(
+          child: Container(
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    "Player X",
+                    style: GoogleFonts.pressStart2p(color: Colors.white),
+                  ),
+                  Text(
+                    "Player O",
+                    style: GoogleFonts.pressStart2p(color: Colors.white),
+                  ),
+                ],
               ),
             ),
-          );
-        },
-      ),
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: GridView.builder(
+            itemCount: 9,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3),
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {
+                  _onTap(index);
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey.shade700)),
+                  child: Center(
+                    child: Text(
+                      displayXO[index],
+                      style: const TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        Expanded(child: Container()),
+      ]),
     );
   }
 
